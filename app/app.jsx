a0c99ec01c8fb121f18300985481295d629be5d1
Martin Simard <Martin Simard>
2016-11-30 14:41:30 -0500

    Support foundation settings

--- app/app.jsx
+++ app/app.jsx
@@ -6,10 +6,10 @@ var Timer = require('Timer');
 var Countdown = require('Countdown');
 
 // Load foundation
-require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
 require('style!css!sass!applicationStyles');
 
-$(document).foundation();
 
 ReactDOM.render(
   <Router history={hashHistory}>
